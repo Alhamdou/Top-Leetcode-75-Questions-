d285cac0a1936a6f4d6167ad619d362c5b268049
Alhamdou <jallowalhamdou.9@gmail.com>
2022-01-09 10:58:48 +0530

    changes

--- containsduplicate.py
+++ containsduplicate.py
@@ -1,5 +1,16 @@
 
 
+
+
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+
 class Solution(object):
     def containsduplicate(self, nums):
         hashset = set()
